5f54c155f4c1591261ff7a329f26ef4c92781ebb
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-05-04 10:52:13 +0500

    connect to the database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-    .then((data) => { 
-      console.log(`Mongodb connected with server: ${data.connection.host}`);
-    }).catch((err)=>{
-      console.log(err)
-    })
+  mongoose.connect(process.env.DB_URI).then((data) => {
+    console.log("Connection to MongoDB Successful");
+  });
 };
 
 module.exports = connectDatabase;
